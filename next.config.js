6c3be165aa32ba9401e6b34e5ec1876f3be043cc
cuvii <xw5427@gmail.com>
2020-11-06 17:27:42 +0800

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -9,35 +9,41 @@ const themeVariables = lessToJS(
   fs.readFileSync(path.resolve(__dirname, "./styles/antd.less"), "utf8")
 );
 
-const plugins = [
-  withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables,
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === "function") {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === "function" ? [] : origExternals),
-        ];
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-        config.module.rules.unshift({
-          test: antStyles,
-          use: "null-loader",
-        });
-      }
-      return config;
-    },
-  }),
+const plugins = [
+  withBundleAnalyzer(
+    withLess({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables,
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === "function") {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === "function" ? [] : origExternals),
+          ];
+
+          config.module.rules.unshift({
+            test: antStyles,
+            use: "null-loader",
+          });
+        }
+        return config;
+      },
+    })
+  ),
 ];
 module.exports = withPlugins(plugins);
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import React from "react";
 import '../styles/antd.less'
+import { Reset } from "styled-reset";
 
 export default function App({ Component, pageProps }) {
     return (
       <ThemeProvider theme={{}}>
+        <Reset/>
         <Component {...pageProps} />
       </ThemeProvider>
     );
